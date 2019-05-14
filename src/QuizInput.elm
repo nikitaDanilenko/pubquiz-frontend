b066a9e004f6c417ce912280eea6a1af0eed1f98
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2019-05-14 21:40:53 +0200

    Add most quiz functions

--- src/QuizInput.elm
+++ src/QuizInput.elm
@@ -1,12 +1,13 @@
 module QuizInput exposing ( .. )
 
 import Browser
-import Html exposing              ( Html, div, text, input, button )
+import Html exposing              ( Html, div, text, input, button, textarea )
 import Html.Attributes exposing   ( id, autocomplete, class )            
 import Html.Events exposing       ( onInput, onClick )
 import Html.Events.Extra exposing ( onEnter )
 import Http
 import Http exposing              ( Error, get, emptyBody, post )
+import Json.Encode as Encode
 
 import Constants exposing         ( serverLocation )
 
@@ -24,6 +25,8 @@ type alias Model =
         user : User,
         password : Password,
         quizzes : List QuizName,
+        editing : QuizName,
+        currentPoints : String,
         displayState : DisplayState,
         errorMsg : String
     }
@@ -32,32 +35,47 @@ initialModel : () -> (Model, Cmd Msg)
 initialModel () = ({ user = "",
                      password = "", 
                      quizzes = [], 
+                     editing = "",
+                     currentPoints = "",
                      displayState = Initial, 
                      errorMsg = "" 
                      }, Cmd.none)
 
 type Msg = GetAll 
-         | GetSingle QuizName 
          | GotAll (Result Http.Error String)
-         | PostUpdate Quiz 
-         | Lock Quiz 
+         | GetSingle QuizName
+         | GotSingle (Result Http.Error String)
+         | PostUpdate QuizName String
+         | AcknowledgeLock
+         | Lock QuizName
          | SetUser User
          | SetPassword Password
+         | SetPoints String
          | LocationChange
+         | Updated (Result Http.Error ())
 
 type alias User = String 
 type alias Password = String
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = case msg of 
-    SetUser u        -> ({ model | user = u }, Cmd.none)
-    SetPassword p    -> ({ model | password = p}, Cmd.none)
-    GetAll           -> ({ model | displayState = Authenticating }, getAll)
+    SetUser u               -> ({ model | user = u }, Cmd.none)
+    SetPassword p           -> ({ model | password = p}, Cmd.none)
+    GetAll                  -> ({ model | displayState = Authenticating }, getAll)
     -- todo: Pretty-print the actual error.
-    GotAll (Err err) -> ({ model | errorMsg = "An error occurred"}, Cmd.none)
-    GotAll (Ok text) -> ({ model | quizzes = String.lines text, displayState = Selecting }, 
-                         Cmd.none)
-    _                -> (model, Cmd.none)
+    GotAll (Err err)        -> ({ model | errorMsg = "An error occurred"}, Cmd.none)
+    GotAll (Ok text)        -> ({ model | quizzes = String.lines text, displayState = Selecting }, 
+                                Cmd.none)
+    GetSingle qName         -> ({ model | editing = qName }, 
+                                 getSingle qName)
+    -- todo: Pretty-print the actual error.
+    GotSingle (Err err)     -> ({ model | errorMsg = "An error occurred"}, Cmd.none)
+    GotSingle (Ok text)     -> ({ model | currentPoints = text,
+                                          displayState = Editing }, Cmd.none)
+    AcknowledgeLock         -> ({ model | displayState = ConfirmingLock}, Cmd.none)
+    SetPoints points        -> ({ model | currentPoints = points}, Cmd.none)
+    PostUpdate qName points -> (model, postUpdate qName points)
+    _                       -> (model, Cmd.none)
 
 view : Model -> Html Msg
 view model = 
@@ -74,23 +92,46 @@ view model =
                         , input [ autocomplete True, onInput SetPassword, onEnter GetAll ] []
                       ],
                   div [ id "fetchButton" ]
-                       [ button [ class "button", onClick GetAll ] [ text "Login" ] ],
+                      [ button [ class "button", onClick GetAll ] [ text "Login" ] ],
                   div [ id "errorLabel" ] [ text md.errorMsg ]
                 ]
 
         selectionView : Model -> Html Msg
         selectionView md =
             let mkButton : QuizName -> Html Msg
-                mkButton name = button [ class "quizButton", onClick (GetSingle (mkFullPath name)) ] 
+                mkButton name = button [ class "quizButton", 
+                                         onClick (GetSingle name) ] 
                                        [ text name ]
             in div [ id "allQuizzesMain" ]
-                   (List.map mkButton md.quizzes)
+                   (List.map mkButton (List.filter (\q -> not (String.isEmpty q)) md.quizzes))
+
+        editingView : Model -> Html Msg
+        editingView md =
+            div [ id "singleQuiz" ]
+                [ textarea [ id "singleQuizArea", onInput SetPoints ] [ text md.currentPoints ],
+                  button [ class "button", onClick GetAll ] [ text "Back" ],
+                  button [ class "button", onClick AcknowledgeLock ] [ text "Lock" ],
+                  button [ class "button", onClick (PostUpdate md.editing md.currentPoints) ]
+                         [ text "Update" ]
+                          ]
+
+        confirmView : Model -> Html Msg
+        confirmView md =
+            div [ id "confirmView" ]
+                [ text (String.concat ["You are about to lock ", md.editing, ". ",
+                                       "This cannot be undone. Please confirm. "]),
+                  button [ class "button", onClick (GetSingle md.editing) ]
+                         [ text "Abort" ],
+                  button [ class "button", onClick (Lock md.editing) ]
+                         [ text "Yes, lock" ]
+                ]
 
         currentView = case model.displayState of
             Authenticating -> authenticationView
             Initial -> authenticationView
-            Editing -> authenticationView
+            Editing -> editingView
             Selecting -> selectionView
+            ConfirmingLock -> confirmView
      in currentView model
 
 mkFullPath : String -> String
@@ -98,6 +139,23 @@ mkFullPath str = String.concat [serverLocation, str]
 
 getAll : Cmd Msg
 getAll = Http.get { url = mkFullPath "all", expect = Http.expectString GotAll }
+
+getSingle : String -> Cmd Msg
+getSingle quizName = Http.get { 
+        url = mkFullPath (String.concat [ "getQuizData?quiz=", quizName ]),
+        expect = Http.expectString GotSingle
+    }
+
+postUpdate : QuizName -> String -> Cmd Msg
+postUpdate quizName points = 
+    Http.post {
+        url = mkFullPath "update",
+        body = Http.stringBody "application/x-www-form-urlencoded"  
+                               (String.concat [
+                                "quiz=", quizName, "&rounds=", (String.replace "\n" "%0A" points)
+                                ]),
+        expect = Http.expectWhatever Updated
+    }
 
 type alias QuizName = String
 
@@ -119,3 +177,4 @@ type DisplayState = Initial -- The state at the beginning of the application.
                   | Editing -- The view where one sees the current quiz values and can update those.
                   | Authenticating -- The view presented for the authentication of a user.
                   | Selecting -- In this view you see all available quizzes.
+                  | ConfirmingLock
