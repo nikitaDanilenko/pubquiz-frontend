a907de805b398300d3582be61ee8bf72522a615c
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2019-05-15 17:45:20 +0200

    Move parameter and api names to constants

--- src/Constants.elm
+++ src/Constants.elm
@@ -1,4 +1,23 @@
 module Constants exposing ( .. )
 
 serverLocation : String
-serverLocation = "https://www.danilenko.io:8000/api/quiz/"
+--serverLocation = "https://www.danilenko.io:8000/api/quiz/"
+serverLocation = "http://localhost:9000/api/quiz/"
+
+quizParam : String
+quizParam = "quiz"
+
+roundsParam : String
+roundsParam = "rounds"
+
+newApi : String
+newApi = "create"
+
+lockApi : String
+lockApi = "lock"
+
+updateApi : String
+updateApi = "update"
+
+allApi : String
+allApi = "all"
