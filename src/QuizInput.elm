c760ebc4f2bfd4e01b001e3de6c4728d32dd9108
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2019-05-15 17:45:54 +0200

    Use constants; start work on 'new quiz' service

--- src/QuizInput.elm
+++ src/QuizInput.elm
@@ -7,7 +7,7 @@ import Http
 import Http exposing         ( get, emptyBody, post )
 import Json.Encode as Encode
 
-import Constants exposing    ( serverLocation )
+import Constants exposing    ( .. )
 import Model exposing        ( .. )
 import Views exposing        ( .. )
 
@@ -66,31 +66,47 @@ mkFullPath str = String.concat [serverLocation, str]
 
 getAll : Cmd Msg
 getAll = Http.get { 
-    url = mkFullPath "all", 
+    url = mkFullPath allApi, 
     expect = Http.expectString GotAll 
   }
 
 getSingle : String -> Cmd Msg
 getSingle quizName = Http.get { 
-        url = mkFullPath (String.concat [ "getQuizData?quiz=", quizName ]),
+        url = mkFullPath (String.concat [ "getQuizData?", mkParam quizParam quizName ]),
         expect = Http.expectString GotSingle
     }
 
 postUpdate : QuizName -> String -> Cmd Msg
 postUpdate quizName points = 
     Http.post {
-        url = mkFullPath "update",
-        body = encodeBody (String.concat [
-                                "quiz=", quizName, "&rounds=", (String.replace "\n" "%0A" points)
-                                ]),
+        url = mkFullPath updateApi,
+        body = encodeBody (mkParams [(quizParam, quizName), 
+                                     (roundsParam, String.replace "\n" "%0A" points)]),
         expect = Http.expectWhatever Updated
     }
 postLock : QuizName -> Cmd Msg
 postLock quizName = Http.post {
-    url = mkFullPath "lock",
-    body = encodeBody (String.concat ["quiz=", quizName]),
+    url = mkFullPath lockApi,
+    body = encodeBody (mkParam quizParam quizName),
     expect = Http.expectWhatever Locked
   }
+
+createNew : QuizName -> Cmd Msg
+createNew quizName = Http.post {
+    url = mkFullPath newApi,
+    body = encodeBody (mkParam quizParam quizName),
+    expect = Http.expectWhatever Locked
+  }
+
+type alias RestParam = String
+type alias RestValue = String
+type alias RestKey = String
+
+mkParam : RestKey -> RestValue -> RestParam
+mkParam key value = String.concat [key, "=", value]
+
+mkParams : List (RestKey, RestValue) -> RestParam
+mkParams kvs = String.concat (List.intersperse "&" (List.map (\(k, v) -> mkParam k v) kvs))
 
 encodeBody : String -> Http.Body
 encodeBody = Http.stringBody "application/x-www-form-urlencoded"
