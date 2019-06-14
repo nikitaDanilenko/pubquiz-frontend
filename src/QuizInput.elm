c58a7de8cba8f21052f216b5ce8009ed5e7d9321
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2019-06-14 07:50:16 +0200

    Reset feedback label after successful fetch

--- src/QuizInput.elm
+++ src/QuizInput.elm
@@ -41,7 +41,8 @@ update msg model = case msg of
 
     GotAll (Err err)        -> ({ model | feedback = errorToString err}, Cmd.none)
     GotAll (Ok text)        -> ({ model | quizzes = String.lines text, 
-                                          displayState = Selecting }, 
+                                          displayState = Selecting,
+                                          feedback = "" }, 
                                 Cmd.none)
     
     GetSingle qName         -> ({ model | editing = qName, feedback = "" }, getSingle qName)
