6b0c8a7327c1e73787c4e214466f73ee9a2284f2
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2019-08-05 16:52:52 +0200

    Add team name checker to validity

--- src/Validity.elm
+++ src/Validity.elm
@@ -3,20 +3,25 @@ module Validity exposing ( .. )
 type alias Validity = 
     {
         serverTextOK : Bool,
-        pointsValid : Bool
+        pointsValid : Bool,
+        teamNamesValid : Bool
     }
 
 default : Validity
 default = {
         serverTextOK = False,
-        pointsValid = True
+        pointsValid = True,
+        teamNamesValid = True
     }
 
 isValid : Validity -> Bool
-isValid v = List.all (\x -> x) [v.serverTextOK, v.pointsValid]
+isValid v = List.all (\x -> x) [v.serverTextOK, v.pointsValid, v.teamNamesValid]
 
 updatePoints : Bool -> Validity -> Validity
 updatePoints b v = { v | pointsValid = b }
 
 updateServerText : Bool -> Validity -> Validity
 updateServerText b v = { v | serverTextOK = b }
+
+updateTeamNames : Bool -> Validity -> Validity
+updateTeamNames b v = { v | teamNamesValid = b }
