390204d8910ebda079e392d9c8fa464a57a57f44
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-02-23 16:31:04 +0100

    Adjust model to use quiz ratings

--- src/Model.elm
+++ src/Model.elm
@@ -1,149 +1,194 @@
-module Model exposing ( .. )
+module Model exposing (..)
 
 import Copy exposing (LabelsField)
-import Http exposing     ( Error ( .. ) )
+import Http exposing (Error(..))
+import NewUser exposing (NewUser, NewUserField)
+import QuizRatings exposing (QuizRatings)
+import Types exposing (Activity(..), DbQuizId, Header, Labels, Password, Place, QuizDate, QuizInfo, QuizName, QuizPDN, QuizSettings, Ratings, UserName)
+import Validity exposing (Validity)
 
-import NewUser exposing  ( NewUser, NewUserField )
-import Quiz exposing     ( Quiz )
-import Types exposing (Labels, Password, Place, QuizDate, QuizName, QuizPDN, QuizSettings, UserName)
-import Validity exposing ( Validity )
-
-type alias Model = 
-    {
-        user : UserName,
-        password : Password,
-        oneWayHash : String,
-        quizzes : List QuizName,
-        editing : QuizName,
-        currentQuiz : Quiz,
-        isValidQuizUpdate : Validity,
-        displayState : DisplayState,
-        createQuizPDN : QuizPDN,
-        quizSettings : QuizSettings,
-        newUser : NewUser,
-        feedback : String
+-- todo: split model in various models? This may simplify the handling of default values.
+type alias Model =
+    { user : UserName
+    , password : Password
+    , oneWayHash : String
+    , quizzes : List QuizName
+    , currentQuizInfo : QuizInfo
+    , currentQuizSettings : QuizSettings
+    , currentQuizRatings : QuizRatings
+    , isValidQuizUpdate : Validity
+    , displayState : DisplayState
+    , newUser : NewUser
+    , feedback : String
     }
 
-initialModelFunction : () -> (Model, Cmd Msg)
-initialModelFunction () = (initialModel, Cmd.none)
+
+initialModelFunction : () -> ( Model, Cmd Msg )
+initialModelFunction () =
+    ( initialModel, Cmd.none )
+
 
 initialModel : Model
-initialModel = { user = "",
-                 password = "",
-                 oneWayHash = "",
-                 quizzes = [], 
-                 editing = "",
-                 currentQuiz = Quiz.empty,
-                 isValidQuizUpdate = Validity.default,
-                 displayState = Initial,
-                 createQuizPDN = defaultQuizPDN,
-                 quizSettings = defaultQuizSettings,
-                 newUser = NewUser.emptyUser,
-                 feedback = "" 
-               }
+initialModel =
+    { user = ""
+    , password = ""
+    , oneWayHash = ""
+    , quizzes = []
+    , currentQuizInfo = defaultQuizInfo
+    , currentQuizSettings = defaultQuizSettings
+    , currentQuizRatings = QuizRatings.empty
+    , isValidQuizUpdate = Validity.default
+    , displayState = Initial
+    , newUser = NewUser.emptyUser
+    , feedback = ""
+    }
+
+
+defaultQuizInfo : QuizInfo
+defaultQuizInfo =
+    { quizId = -1
+    , identifier = defaultQuizPDN
+    , active = Inactive
+    }
+
 
 defaultQuizPDN : QuizPDN
-defaultQuizPDN = {
-    place = "",
-    date = { year = 2100, month = 1, day = 1 },
-    name = defaultLabels.mainLabel
-  }
+defaultQuizPDN =
+    { place = ""
+    , date = "2100-01-01"
+    , name = defaultLabels.mainLabel
+    }
+
 
 defaultQuizSettings : QuizSettings
-defaultQuizSettings = {
-    rounds = defaultRounds,
-    numberOfTeams = defaultNumberOfTeams,
-    labels = defaultLabels
-  }
+defaultQuizSettings =
+    { rounds = defaultRounds
+    , numberOfTeams = defaultNumberOfTeams
+    , labels = defaultLabels
+    }
+
 
 defaultLabels : Labels
-defaultLabels = {
-    roundLabel = "Runde",
-    teamLabel = "Gruppe",
-    ownPointsLabel = "Erreichte Punkte",
-    maxReachedLabel = (String.concat ["Erreichte H", String.fromChar (Char.fromCode 246), "chstpunktzahl"]),
-    maxReachableLabel = "Erreichbare Punkte",
-    backToChartView = "Gesamtwertung",
-    mainLabel = "Quiz",
-    ownPageLabel = "Eigene Punkte",
-    viewPrevious = "Alle Quizzes",
-    cumulativeLabel = "Gesamtpunktzahl",
-    individualRoundsLabel = "Punkte pro Runde",
-    progressionLabel = "Verlauf",
-    placementLabel = "Platzierung",
-    placeLabel = "Platz",
-    pointsLabel = "Punkte",
-    roundWinnerLabel = "Rundensieger"
-  }
+defaultLabels =
+    { roundLabel = "Runde"
+    , teamLabel = "Gruppe"
+    , ownPointsLabel = "Erreichte Punkte"
+    , maxReachedLabel = String.concat [ "Erreichte H", String.fromChar (Char.fromCode 246), "chstpunktzahl" ]
+    , maxReachableLabel = "Erreichbare Punkte"
+    , backToChartView = "Gesamtwertung"
+    , mainLabel = "QuizRatings"
+    , ownPageLabel = "Eigene Punkte"
+    , viewPrevious = "Alle Quizzes"
+    , cumulativeLabel = "Gesamtpunktzahl"
+    , individualRoundsLabel = "Punkte pro Runde"
+    , progressionLabel = "Verlauf"
+    , placementLabel = "Platzierung"
+    , placeLabel = "Platz"
+    , pointsLabel = "Punkte"
+    , roundWinnerLabel = "Rundensieger"
+    }
+
 
 defaultNumberOfTeams : Int
-defaultNumberOfTeams = 8
+defaultNumberOfTeams =
+    8
+
 
 defaultQuestionNumber : Int
-defaultQuestionNumber = 8
+defaultQuestionNumber =
+    8
+
 
 defaultRounds : List Int
-defaultRounds = List.repeat 4 defaultQuestionNumber
+defaultRounds =
+    List.repeat 4 defaultQuestionNumber
 
-type Edited = ContentsE | LabelsE
+
+type Edited
+    = ContentsE
+    | LabelsE
+
+
 
 {- The different types of possible states the page can transition. -}
-type DisplayState = Initial -- The state at the beginning of the application.
-                  | Editing Edited -- The view where one sees the current quiz values and can update those.
-                  | Authenticating -- The view presented for the authentication of a user.
-                  | Selecting -- In this view you see all available quizzes.
-                  | ConfirmingLock
-                  | CreatingQuiz
-                  | CreatingUser
-
-type alias ErrorOr a = Result Http.Error a
-
-type TeamUpdateSetting = InitialTU | IntermediateTU
-
-type ResponseWithFeedback = GotAll
-                          | GotSingleQuiz
-                          | GotSingleLabels
-                          | Logged
-
-type ResponsePure = Updated
-                  | Locked
-                  | CreatedQuiz
-                  | CreatedUser
 
 
-type Msg = GetAll 
-         | GetSingle QuizName
-         | PostUpdate QuizName String
-         | AcknowledgeLock
-         | Lock QuizName
-         | SetUser UserName
-         | SetPassword Password
-         | SetTeamsInQuiz TeamUpdateSetting String
-         | UpdatePoints Int Int String -- Points for round, team, value.
-         | SetMaxPoints Int String    -- Points for round, value
-         | UpdateQuestions Int String
-         | AddRound
-         | SetRoundsNumber String
-         | StartCreatingQuiz
-         | StartCreatingUser
-         | CreateQuiz
-         | CreateUser
-         | SetNewQuizName QuizName
-         | SetNewUserParam NewUserField String
-         | Login
-         | LabelsUpdate LabelsField String
-         | SetTeamName Int String
-         | GetLabels
-         | PostQuizSettingsUpdate QuizName (List Int) Int Labels
-         | ResponseF ResponseWithFeedback (ErrorOr String)
-         | ResponseP ResponsePure (ErrorOr ())
+type DisplayState
+    = Initial -- The state at the beginning of the application.
+    | Editing Edited -- The view where one sees the current quiz values and can update those.
+    | Authenticating -- The view presented for the authentication of a user.
+    | Selecting -- In this view you see all available quizzes.
+    | ConfirmingLock
+    | CreatingQuiz
+    | CreatingUser
 
+
+type alias ErrorOr a =
+    Result Http.Error a
+
+
+type TeamUpdateSetting
+    = InitialTU
+    | IntermediateTU
+
+
+type ResponseWithFeedback
+    = GotAll
+    | GotSingleQuiz
+    | GotSingleLabels
+    | Logged
+
+
+type ResponsePure
+    = Updated
+    | Locked
+    | CreatedQuiz
+    | CreatedUser
+
+
+type Msg
+    = GetAll
+    | GetSingle QuizName
+    | PostUpdate QuizName String
+    | AcknowledgeLock
+    | LockQuiz QuizName
+    | SetUser UserName
+    | SetPassword Password
+    | SetTeamsInQuiz TeamUpdateSetting String
+    | UpdatePoints Int Int String -- Points for round, team, value.
+    | SetMaxPoints Int String -- Points for round, value
+    | UpdateQuestions Int String
+    | AddRound
+    | SetRoundsNumber String
+    | StartCreatingQuiz
+    | StartCreatingUser
+    | CreateQuiz
+    | CreateUser
+    | SetNewQuizName QuizName
+    | SetNewUserParam NewUserField String
+    | Login
+    | LabelsUpdate LabelsField String
+    | SetTeamName Int String
+    | GetLabels
+    | PostQuizSettingsUpdate QuizName QuizSettings
+    | ResponseF ResponseWithFeedback (ErrorOr String)
+    | ResponseP ResponsePure (ErrorOr ())
 
 
 errorToString : Http.Error -> String
-errorToString err = case err of
-     BadUrl url   -> String.concat [ "Bad URL: ", url ]
-     Timeout      -> "Timeout"
-     NetworkError -> "Network error"
-     BadStatus s  -> String.concat [ "Bad status: ", String.fromInt s ]
-     BadBody str  -> String.concat [ "Bad body: ", str ]
+errorToString err =
+    case err of
+        BadUrl url ->
+            String.concat [ "Bad URL: ", url ]
+
+        Timeout ->
+            "Timeout"
+
+        NetworkError ->
+            "Network error"
+
+        BadStatus s ->
+            String.concat [ "Bad status: ", String.fromInt s ]
+
+        BadBody str ->
+            String.concat [ "Bad body: ", str ]
