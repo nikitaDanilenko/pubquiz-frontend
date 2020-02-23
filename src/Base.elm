dae1329b4ea269ad92c918c8a4de5a4a080eb2b1
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-02-23 12:53:03 +0100

    Adjust conversion to simplified day type

--- src/Base.elm
+++ src/Base.elm
@@ -1,16 +1,12 @@
 module Base exposing ( .. )
 
-import Date exposing (Date, day, fromCalendarDate, monthNumber, numberToMonth, year)
+import Date exposing (Date, fromIsoString, toIsoString)
 import Types exposing (Day)
 
 type alias SessionKey = String
 
-dayToDate : Day -> Date
-dayToDate d = fromCalendarDate d.year (numberToMonth d.month) d.day
+dayToDate : Day -> Result String Date
+dayToDate = fromIsoString
 
 dateToDay : Date -> Day
-dateToDay date = {
-    year = year date,
-    month = monthNumber date,
-    day = day date
-    }
+dateToDay = toIsoString
