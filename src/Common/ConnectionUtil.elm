47a585ba35cd0d96898696b28181ccbb52fe05d6
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-03-08 15:13:13 +0100

    Extract another utility function

--- src/Common/ConnectionUtil.elm
+++ src/Common/ConnectionUtil.elm
@@ -2,7 +2,7 @@ module Common.ConnectionUtil exposing (..)
 
 import Common.Constants exposing (getLabelsApi, getQuizInfoApi, getQuizRatingsApi)
 import Common.Types exposing (DbQuizId, Labels, QuizInfo, QuizRatings, jsonDecLabels, jsonDecQuizInfo, jsonDecQuizRatings)
-import Common.Util exposing (getMsg)
+import Common.Util as Util exposing (getMsg)
 import Input.Model exposing (ErrorOr)
 
 
@@ -19,3 +19,7 @@ getQuizInfoWith f =
 getQuizRatingsWith : (ErrorOr QuizRatings -> msg) -> DbQuizId -> Cmd msg
 getQuizRatingsWith f =
     getMsg getQuizRatingsApi f jsonDecQuizRatings
+
+useOrFetchWith : (DbQuizId -> Cmd msg) -> Maybe a -> DbQuizId -> Cmd msg
+useOrFetchWith dft mA qid =
+    Util.foldMaybe (dft qid) (always Cmd.none) mA
