68c2e1e5e79f06de26c46eba9ee43913f7721f5d
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-03-11 23:34:34 +0100

    #6: Separate parametric quiz setting functions

---
+++ src/Input/QuizValuesUtil.elm
@@ -0,0 +1,173 @@
+module Input.QuizValuesUtil exposing (..)
+
+import Common.Copy exposing (LabelsField(..))
+import Common.Types exposing (Labels, Place, QuizDate, QuizName, QuizSettings)
+import Html exposing (Html, div, input, label, text)
+import Html.Attributes exposing (class, for, id, placeholder, step, type_, value)
+import Html.Events exposing (onInput)
+
+
+type alias MsgFunctions msg =
+    { labelsUpdate : LabelsField -> String -> msg
+    , setQuizName : QuizName -> msg
+    , setQuizDate : QuizDate -> msg
+    , setQuizPlace : Place -> msg
+    , setRoundsNumber : String -> msg
+    , setTeamsInQuiz : String -> msg
+    }
+
+
+mkCreationFormWith :
+    MsgFunctions msg
+    -> QuizSettings
+    -> Html.Attribute msg
+    -> Labels
+    -> List (Html msg)
+mkCreationFormWith msgFs quizSettings createOnEnter labels =
+    let
+        associations =
+            [ ( "Label for rounds", RoundField, labels.roundLabel )
+            , ( "Label for teams", TeamField, labels.teamLabel )
+            , ( "Label for own points", OwnPointsField, labels.ownPointsLabel )
+            , ( "Label for maximum reached points", MaxReachedField, labels.maxReachedLabel )
+            , ( "Label for maximum reachable points", MaxReachableField, labels.maxReachableLabel )
+            , ( "Label for 'back to chart'", BackField, labels.backToChartView )
+            , ( "Label for own page", OwnPageField, labels.ownPageLabel )
+            , ( "Label for 'view quizzes' button", ViewPreviousField, labels.viewPrevious )
+            , ( "Label for cumulative points", CumulativeField, labels.cumulativeLabel )
+            , ( "Label for individual points", IndividualRoundsField, labels.individualRoundsLabel )
+            , ( "Label for progression", ProgressionField, labels.progressionLabel )
+            , ( "Label for placement", PlacementField, labels.placementLabel )
+            , ( "Label for place", PlaceField, labels.placeLabel )
+            , ( "Label for points", PointsField, labels.pointsLabel )
+            , ( "Label for round winner", RoundWinnerField, labels.roundWinnerLabel )
+            ]
+
+        mkInput : String -> LabelsField -> String -> Html msg
+        mkInput lbl fld dft =
+            div [ id (createIdByField fld) ]
+                [ label [] [ text lbl ]
+                , input [ onInput (msgFs.labelsUpdate fld), type_ "text", value dft, createOnEnter ] []
+                ]
+
+        mkIdentifierPart : String -> String -> String -> String -> String -> (String -> msg) -> Html msg
+        mkIdentifierPart divId labelFor description inputType example onInputFct =
+            div [ id divId ]
+                [ label [ for labelFor ] [ text description ]
+                , input [ onInput onInputFct, type_ inputType, createOnEnter, placeholder example ] []
+                ]
+    in
+    [ mkIdentifierPart "quizNameDiv" "quizName" "Quiz name" "text" "e.g. Quiz" msgFs.setQuizName
+    , mkIdentifierPart "quizDateDiv" "quizDate" "Quiz date" "date" "e.g. 2020-01-01" msgFs.setQuizDate
+    , mkIdentifierPart "quizPlaceDiv" "quizPlace" "Quiz place" "text" "e.g. Cheers" msgFs.setQuizPlace
+    , div [ id "roundsNumberDiv" ]
+        [ label [ for "roundsNumber" ]
+            [ text "Number of rounds" ]
+        , input
+            [ onInput msgFs.setRoundsNumber
+            , class "roundsSpinner"
+            , type_ "number"
+            , min "1"
+            , step "1"
+            , createOnEnter
+            , value (String.fromInt (List.length quizSettings.rounds))
+            ]
+            []
+        ]
+    , div [ id "questionLabel" ]
+        [ label [ id "questionsPerRound" ]
+            [ text "Questions per round" ]
+        ]
+    , div [ id "questionArea" ]
+        [ mkQuestionsForm createOnEnter quizSettings.rounds ]
+    , div [ id "teamNumberArea" ]
+        [ label [ for "teamNumber" ] [ text "Number of teams" ]
+        , input
+            [ onInput msgFs.setTeamsInQuiz
+            , class "teamsSpinner"
+            , type_ "number"
+            , min "1"
+            , createOnEnter
+            , value (String.fromInt quizSettings.numberOfTeams)
+            ]
+            []
+        ]
+    , div [ id "labelsForm" ]
+        (List.map (\( lbl, fld, dft ) -> mkInput lbl fld dft) associations)
+    ]
+
+
+createIdByField : LabelsField -> String
+createIdByField fld =
+    case fld of
+        RoundField ->
+            "roundField"
+
+        TeamField ->
+            "teamField"
+
+        OwnPointsField ->
+            "ownPointsField"
+
+        MaxReachedField ->
+            "maxReachedField"
+
+        MaxReachableField ->
+            "maxReachableField"
+
+        BackField ->
+            "backField"
+
+        OwnPageField ->
+            "ownPageField"
+
+        ViewPreviousField ->
+            "viewPreviousField"
+
+        CumulativeField ->
+            "cumulativeField"
+
+        IndividualRoundsField ->
+            "individualRoundsField"
+
+        ProgressionField ->
+            "progressionField"
+
+        PlacementField ->
+            "placementField"
+
+        PlaceField ->
+            "placeField"
+
+        PointsField ->
+            "pointsField"
+
+        RoundWinnerField ->
+            "roundWinnerField"
+
+
+mkQuestionsForm : (Int -> String -> msg) -> Html.Attribute msg -> List Int -> Html msg
+mkQuestionsForm updateQuestions createOnEnter rs =
+    div [ id "perRound" ]
+        (List.concat
+            (List.indexedMap
+                (\i qs ->
+                    [ div [ class "roundQuestionLine" ]
+                        [ label [ class "roundNumber" ]
+                            [ text (String.join " " [ "Round", String.fromInt (1 + i) ]) ]
+                        , input
+                            [ value (String.fromInt qs)
+                            , onInput (updateQuestions i)
+                            , class "questionSpinner"
+                            , type_ "number"
+                            , min "1"
+                            , step "1"
+                            , createOnEnter
+                            ]
+                            []
+                        ]
+                    ]
+                )
+                rs
+            )
+        )
