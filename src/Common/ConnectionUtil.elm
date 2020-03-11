522ebc9a9eb71706804d3e36fb1bf508f79ba084
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-03-11 23:15:21 +0100

    #6: Extract more utility functions

--- src/Common/ConnectionUtil.elm
+++ src/Common/ConnectionUtil.elm
@@ -5,6 +5,7 @@ import Common.Types exposing (DbQuizId, Labels, QuizInfo, QuizRatings, jsonDecLa
 import Common.Util as Util exposing (getMsg)
 import Html exposing (Attribute, Html, div, form, input, label, text)
 import Html.Attributes exposing (action, for, id, type_)
+import Http exposing (Error(..))
 import Input.Model exposing (ErrorOr)
 
 
@@ -28,12 +29,37 @@ useOrFetchWith dft mA qid =
     Util.foldMaybe (dft qid) (always Cmd.none) mA
 
 
+encodeBody : String -> Http.Body
+encodeBody =
+    Http.stringBody "application/x-www-form-urlencoded"
+
+
 linkButton : String -> List (Attribute msg) -> List (Html msg) -> Html msg
 linkButton link attrs children =
     form [ action link ]
         [ input (type_ "submit" :: attrs) children ]
 
+
 addFeedbackLabel : String -> Html msg
 addFeedbackLabel feedback =
     div [ id "feedbackArea" ]
         [ label [ for "feedbackLabel" ] [ text feedback ] ]
+
+
+errorToString : Http.Error -> String
+errorToString err =
+    case err of
+        BadUrl url ->
+            String.concat [ "Bad URL: ", url ]
+
+        Timeout ->
+            "Timeout"
+
+        NetworkError ->
+            "Network error"
+
+        BadStatus s ->
+            String.concat [ "Bad status: ", String.fromInt s ]
+
+        BadBody str ->
+            String.concat [ "Bad body: ", str ]
