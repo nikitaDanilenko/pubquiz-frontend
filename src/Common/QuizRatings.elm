69febc7a547e3a87c4c38ea94b0d99118f483c6e
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-03-22 10:23:08 +0100

    #8: Simplify implementation

--- src/Common/QuizRatings.elm
+++ src/Common/QuizRatings.elm
@@ -1,8 +1,11 @@
 module Common.QuizRatings exposing (..)
 
+import Basics.Extra exposing (flip)
+import Common.Copy exposing (updateTeamInfoTeamName)
 import Common.RoundRating as RoundRating
 import Common.Types exposing (DbQuizId, Header, QuizRatings, Ratings, RoundNumber, RoundRating, TeamName, TeamNumber)
 import Common.Util as Util
+import List.Extra exposing (updateIf)
 
 
 empty : QuizRatings
@@ -10,6 +13,7 @@ empty =
     { header = defaultHeader
     , ratings = defaultRatings
     }
+
 
 updateQuizRatingsRatings : QuizRatings -> Ratings -> QuizRatings
 updateQuizRatingsRatings quizRatings ratings =
@@ -19,6 +23,7 @@ updateQuizRatingsRatings quizRatings ratings =
 updateQuizRatingsHeader : QuizRatings -> Header -> QuizRatings
 updateQuizRatingsHeader quizRatings header =
     { quizRatings | header = header }
+
 
 defaultRatings : Ratings
 defaultRatings =
@@ -90,20 +95,10 @@ arePointsValid q =
 
 
 updateTeamName : TeamNumber -> TeamName -> QuizRatings -> QuizRatings
-updateTeamName tNum tName quiz =
-    let
-        inHeader : TeamNumber -> TeamName -> Header -> Header
-        inHeader pos name =
-            List.map
-                (\ti ->
-                    if ti.teamInfoNumber == pos then
-                        { ti | teamInfoName = name }
-
-                    else
-                        ti
-                )
-    in
-    { quiz | header = inHeader tNum tName quiz.header }
+updateTeamName teamNumber teamName quizRatings =
+    quizRatings.header
+        |> updateIf (\teamInfo -> teamInfo.teamInfoNumber == teamNumber) (flip updateTeamInfoTeamName teamName)
+        |> updateQuizRatingsHeader quizRatings
 
 
 addRound : RoundRating -> QuizRatings -> QuizRatings
