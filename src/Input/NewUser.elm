02189e2999727bdee6f5b629f320e8168d1102c5
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-03-23 22:24:53 +0100

    Run formatter, expand abbreviations

--- src/Input/NewUser.elm
+++ src/Input/NewUser.elm
@@ -1,31 +1,45 @@
-module Input.NewUser exposing ( .. )
+module Input.NewUser exposing (..)
 
 import Common.Types exposing (Password, UserName)
 
-type alias NewUser = {
-        user : UserName,
-        password1 : Password,
-        password2 : Password
+
+type alias NewUser =
+    { user : UserName
+    , password1 : Password
+    , password2 : Password
     }
 
-empty : NewUser
-empty = {
-    user = "",
-    password1 = "",
-    password2 = ""  
-  }
 
-type NewUserField = UserField | PasswordField1 | PasswordField2
+empty : NewUser
+empty =
+    { user = ""
+    , password1 = ""
+    , password2 = ""
+    }
+
+
+type NewUserField
+    = UserField
+    | PasswordField1
+    | PasswordField2
+
 
 update : NewUser -> NewUserField -> String -> NewUser
-update nu fld text =
+update newUser fld text =
     case fld of
-        UserField -> { nu | user = text }
-        PasswordField1 -> { nu | password1 = text }
-        PasswordField2 -> { nu | password2 = text }
+        UserField ->
+            { newUser | user = text }
+
+        PasswordField1 ->
+            { newUser | password1 = text }
+
+        PasswordField2 ->
+            { newUser | password2 = text }
+
 
 isValid : NewUser -> Bool
-isValid nu = 
-    not (String.isEmpty nu.user) && 
-    nu.password1 == nu.password2 && 
-    not (String.isEmpty nu.password1)
+isValid newUser =
+    not (String.isEmpty newUser.user)
+        && newUser.password1
+        == newUser.password2
+        && not (String.isEmpty newUser.password1)
