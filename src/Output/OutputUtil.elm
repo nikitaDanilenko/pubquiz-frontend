347a2cd9974d55112084c8a63093a8eb6d05887b
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-03-23 23:52:43 +0100

    #7: Adjust implementation of the server link function

--- src/Output/OutputUtil.elm
+++ src/Output/OutputUtil.elm
@@ -1,6 +1,6 @@
 module Output.OutputUtil exposing (..)
 
-import Common.Constants exposing (serverLocationWithPort)
+import Common.Constants exposing (serverLocationWithFrontendPort)
 import Common.Types exposing (DbQuizId, Labels, QuizIdentifier, QuizInfo)
 import Url.Builder exposing (Root(..))
 
@@ -21,4 +21,8 @@ fragmentUrl parts =
 
 fromServerUrl : List String -> List String -> String
 fromServerUrl pathSegments parts =
-    Url.Builder.custom (CrossOrigin serverLocationWithPort) (pathSegments ++ [ "" ]) [] (Just (Url.Builder.absolute parts []))
+    Url.Builder.custom
+        (CrossOrigin serverLocationWithFrontendPort)
+        (pathSegments ++ [ "" ])
+        []
+        (Just (Url.Builder.absolute parts []))
