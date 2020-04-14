9caac00d8bada581e8c3ab0d71453fb4c5fc1d7d
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-04-15 00:29:13 +0200

    #4: Add computation of cumulative quiz ratings

--- src/Common/QuizRatings.elm
+++ src/Common/QuizRatings.elm
@@ -3,7 +3,7 @@ module Common.QuizRatings exposing (..)
 import Basics.Extra exposing (flip)
 import Common.Copy exposing (updateTeamInfoTeamName)
 import Common.RoundRating as RoundRating
-import Common.Types exposing (DbQuizId, Header, QuizRatings, Ratings, RoundNumber, RoundRating, TeamName, TeamNumber)
+import Common.Types exposing (DbQuizId, Header, QuizRatings, Ratings, RoundNumber, RoundRating, TeamName, TeamNumber, TeamRating)
 import Common.Util as Util
 import List.Extra exposing (updateIf)
 
@@ -66,3 +66,40 @@ addRound r q =
             1 + Maybe.withDefault 0 (List.maximum (List.map Tuple.first q.ratings))
     in
     { q | ratings = q.ratings ++ [ ( rNum, r ) ] }
+
+
+cumulative : QuizRatings -> QuizRatings
+cumulative quizRatings =
+    let
+        addTeamRatings : List TeamRating -> List TeamRating -> List TeamRating
+        addTeamRatings =
+            Util.intersectWith (\x y -> { teamNumber = x.teamNumber, rating = x.rating + y.rating })
+                .teamNumber
+                identity
+                .teamNumber
+                identity
+
+        addRoundRatings : RoundRating -> RoundRating -> RoundRating
+        addRoundRatings x y =
+            { reachableInRound = x.reachableInRound + y.reachableInRound
+            , points = addTeamRatings x.points y.points
+            }
+    in
+    { quizRatings
+        | ratings =
+            quizRatings.ratings
+                |> List.sortBy Tuple.first
+                |> List.map
+                    (Tuple.mapSecond
+                        (\roundRating ->
+                            { roundRating
+                                | points =
+                                    roundRating.points |> List.sortBy .teamNumber
+                            }
+                        )
+                    )
+                |> List.Extra.scanl
+                    (\( roundNumber, rating ) ( _, ratingAcc ) -> ( roundNumber, addRoundRatings rating ratingAcc ))
+                    ( 0, RoundRating.emptyForHeader quizRatings.header )
+                |> List.drop 1
+    }
