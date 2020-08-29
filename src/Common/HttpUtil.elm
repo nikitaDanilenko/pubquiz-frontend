8929c0806094334c6dd6091ba7c325bd644c683d
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-08-29 11:46:57 +0200

    #14: Feat: Add auxiliary expect functions that place an error into an accessible location

---
+++ src/Common/HttpUtil.elm
@@ -0,0 +1,51 @@
+module Common.HttpUtil exposing (..)
+
+import Http exposing (Error(..), Expect, expectStringResponse)
+import Json.Decode as D
+
+
+expectJson : (Result Http.Error a -> msg) -> D.Decoder a -> Expect msg
+expectJson toMsg decoder =
+    expectStringResponse toMsg <|
+        \response ->
+            case response of
+                Http.BadUrl_ url ->
+                    Err (Http.BadUrl url)
+
+                Http.Timeout_ ->
+                    Err Http.Timeout
+
+                Http.NetworkError_ ->
+                    Err Http.NetworkError
+
+                Http.BadStatus_ _ body ->
+                    Err (BadBody body)
+
+                Http.GoodStatus_ _ body ->
+                    case D.decodeString decoder body of
+                        Ok value ->
+                            Ok value
+
+                        Err err ->
+                            Err (BadBody (D.errorToString err))
+
+
+expectWhatever : (Result Http.Error () -> msg) -> Expect msg
+expectWhatever toMsg =
+    expectStringResponse toMsg <|
+        \response ->
+            case response of
+                Http.BadUrl_ url ->
+                    Err (Http.BadUrl url)
+
+                Http.Timeout_ ->
+                    Err Http.Timeout
+
+                Http.NetworkError_ ->
+                    Err Http.NetworkError
+
+                Http.BadStatus_ _ body ->
+                    Err (BadBody body)
+
+                Http.GoodStatus_ _ _ ->
+                    Ok ()
