7f9a80f2125cd6dacba77cb0eaa66c4965c390ff
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2026-05-14 09:43:48 +0200

    Add tests

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,22 +1,90 @@
 module Tests exposing (..)
 
-import Test exposing (..)
+import Api.Types exposing (Team)
 import Expect
+import Fuzz exposing (bool, int, string)
+import Test exposing (..)
+import Util.Team exposing (activeTeams, teamName)
 
 
--- Check out https://package.elm-lang.org/packages/elm-explorations/test/latest to learn more about testing in Elm!
+mkTeam : Int -> String -> Bool -> Team
+mkTeam number name active =
+    { number = number, name = name, active = active }
 
--- todo: Add tests, or remove this file!
+
+teamNameTests : Test
+teamNameTests =
+    describe "teamName"
+        [ test "uses name when non-empty" <|
+            \_ ->
+                Expect.equal "Quizzards" (teamName (mkTeam 1 "Quizzards" True))
+        , test "falls back to Team N when name is empty" <|
+            \_ ->
+                Expect.equal "Team 3" (teamName (mkTeam 3 "" True))
+        , fuzz int "fallback contains team number" <|
+            \n ->
+                let
+                    result =
+                        teamName (mkTeam n "" True)
+                in
+                Expect.equal True (String.startsWith "Team " result)
+        , fuzz string "non-empty name is returned unchanged" <|
+            \name ->
+                let
+                    nonEmpty =
+                        "x" ++ name
+                in
+                Expect.equal nonEmpty (teamName (mkTeam 1 nonEmpty True))
+        ]
+
+
+activeTeamsTests : Test
+activeTeamsTests =
+    describe "activeTeams"
+        [ test "filters out inactive teams" <|
+            \_ ->
+                let
+                    teams =
+                        [ mkTeam 1 "A" True, mkTeam 2 "B" False, mkTeam 3 "C" True ]
+                in
+                Expect.equal [ 1, 3 ] (List.map .number (activeTeams teams))
+        , test "sorts by number" <|
+            \_ ->
+                let
+                    teams =
+                        [ mkTeam 3 "C" True, mkTeam 1 "A" True, mkTeam 2 "B" True ]
+                in
+                Expect.equal [ 1, 2, 3 ] (List.map .number (activeTeams teams))
+        , fuzz bool "inactive team never appears in result" <|
+            \_ ->
+                let
+                    team =
+                        mkTeam 42 "Solo" False
+
+                    result =
+                        activeTeams [ team ]
+                in
+                Expect.equal [] result
+        , fuzz (Fuzz.list (Fuzz.map3 mkTeam int string bool)) "result contains only active teams" <|
+            \teams ->
+                let
+                    result =
+                        activeTeams teams
+                in
+                Expect.equal True (List.all .active result)
+        , fuzz (Fuzz.list (Fuzz.map3 mkTeam int string bool)) "result is sorted by number" <|
+            \teams ->
+                let
+                    numbers =
+                        List.map .number (activeTeams teams)
+                in
+                Expect.equal numbers (List.sort numbers)
+        ]
+
+
 all : Test
 all =
-    describe "A Test Suite"
-        [ test "Addition" <|
-            \_ ->
-                Expect.equal 10 (3 + 7)
-        , test "String.left" <|
-            \_ ->
-                Expect.equal "a" (String.left 1 "abcdefg")
-        , test "This test should fail" <|
-            \_ ->
-                Expect.fail "failed as expected!"
+    describe "Util.Team"
+        [ teamNameTests
+        , activeTeamsTests
         ]
